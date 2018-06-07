3413483c7a6bde0c6431caeedc442c750e34fba3
ADinushika <amalidinushika93@gmail.com>
2018-06-07 15:03:13 +0530

    #9 Create footer.php Page

---
+++ footer.php
@@ -0,0 +1,87 @@
+<footer id="footer">
+            <div class="footer-top">
+                <div class="container text-justify">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 footer-info">
+                            <h3>ADcreations</h3>  
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+
+                            </p>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-links">
+                            <h4>Usefull Links<hr></h4>
+                            <ul>
+                                <li><i class="fa fa-chevron-right"></i>
+                                <a href="#">Home</a>
+                                </li>
+                                <li><i class="fa fa-chevron-right"></i>
+                                <a href="#">About Us</a>
+                                </li>
+                                <li><i class="fa fa-chevron-right"></i>
+                                <a href="#">Services</a>
+                                </li>
+                                <li><i class="fa fa-chevron-right"></i>
+                                <a href="#">Team</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-contact">
+                            <h4>Contact Us<hr></h4> 
+                            <p>
+                                No.205
+                                <br>
+                                Bokundara
+                                <br>
+                                Piliyandala
+                                <br>
+                                Sri Lanka
+                                <br>
+                                <strong>Phone:</strong>
+                                +94 1 234 514 785
+                                <br>
+                                <strong>Email:</strong>
+                                info@gmail.ocm
+                                <br>
+                            </p>
+                            <div class="social-links">
+                                <a class="twitter" href="#">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                                <a class="facebook" href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                                <a class="instagram" href="#">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                                <a class="google-plus" href="#">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 footer-newsletter">
+                            <h4>Our news letter<hr></h4>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+                            <form action="" method="post">
+                                <input name="email" type="email">
+                                <input value="subscribe" type="submit">
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div> 
+            <div class="container">
+                <div class="copyright"><i class="glyphicon glyphicon-copyright-mark"></i>
+                    Copyright
+                    <strong>ADcreations</strong>
+                    .All Rights Reserved
+                </div>
+                <div class="credits">
+                    Designed By 
+                    <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                </div>
+            </div>
+
+        </footer>
