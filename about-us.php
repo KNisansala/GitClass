e66bf173dffc32c040aa8c772221ccf318ddfa27
ADinushika <amalidinushika93@gmail.com>
2018-06-07 14:18:56 +0530

    #5 Create about-us.php Page

---
+++ about-us.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        
+        <script src="js/jquery-3.3.1.js" type="text/javascript"></script>
+        
+    </body>
+</html>
